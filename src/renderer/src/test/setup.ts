9944c85a90af9a81f88225d21ed839bd45cf2b28
kheif <aydinmehmetege@gmail.com>
2026-01-11 09:31:29 +0100

    chore(infra): setup vitest

---
+++ src/renderer/src/test/setup.ts
@@ -0,0 +1,13 @@
+/**
+ * Vitest Test Setup
+ * Global test configuration and mocks
+ */
+
+import '@testing-library/react'
+import { cleanup } from '@testing-library/react'
+import { afterEach } from 'vitest'
+
+// Automatically cleanup after each test
+afterEach(() => {
+  cleanup()
+})
